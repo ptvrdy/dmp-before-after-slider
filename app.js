3edd0694c6310ca458753be4cb3235c4437ab805
ptvrdy <peyton.tvrdy123@gmail.com>
2024-10-09 10:34:04 -0400

    Combined js files

--- app.js
+++ app.js
@@ -82,3 +82,25 @@ function changeImg(increment) {
   slides[currentImg].classList.add('active');
   initBeforeAfterSlider(slides[currentImg]); // Reinitialize slider for the new image
 }
+
+// Start of Copy/Paste Java Script
+// Copy Button Functionality
+function copyText(event) {
+  var button = event.target;
+  var paragraph = button.previousElementSibling;
+  var textToCopy = paragraph.innerText;
+
+  navigator.clipboard.writeText(textToCopy).then(function() {
+      button.innerHTML = '<i class="fa fa-clone" aria-hidden="true"></i> Copied!';
+      setTimeout(function() {
+          button.innerHTML = '<i class="fa fa-clone" aria-hidden="true"></i> Copy';
+      }, 2000);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var buttons = document.querySelectorAll('.button');
+  buttons.forEach(function(button) {
+      button.addEventListener('click', copyText)
+  });
+});
